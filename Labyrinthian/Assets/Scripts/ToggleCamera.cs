c404a3665a6493b36f13252cc2bfecd2a9a8af8d
VampireViktor20 <kylecornw3ll@outlook.com>
2021-11-21 23:06:58 +0000

    Movement & Camera
    - Player Movement & and Camera Views Implemented.
    - Toggle method applied between Camera Views.

---
+++ Labyrinthian/Assets/Scripts/ToggleCamera.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleCamera : MonoBehaviour
+{
+    public Animator anim;
+    public bool toggle;
+    private int status;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        status = 0;
+        
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            ToggleView();
+        }
+    
+       
+    }
+    
+    void ToggleView()
+    {
+        if(status == 0)
+        {
+            FirstPerson();
+        }
+        else if(status == 2)
+        {
+            TopDown();
+        }
+    }
+    void TopDown()
+    {
+        status = 0;
+        anim.Play("ToggleOff");
+    }
+
+    void FirstPerson()
+    {
+        status = 2;
+        anim.Play("ToggleOn");
+    }
+
+
+}
