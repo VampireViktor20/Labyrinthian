b881ce79915007ecbc7cb7b7e538b1fb3c3912fc
VampireViktor20 <kylecornw3ll@outlook.com>
2022-01-06 23:15:55 +0000

    Timer Mechanics 1
    - Integrated on screen countdown timer.
    - Maze change after timer reaches 0
    - Needs altering for smoothness.
    - Next is player camera updates.

---
+++ Labyrinthian/Assets/Scripts/Timer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Maze maze;
+    public float timeValue = 5f;
+    public Text timer;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (timeValue > 0f)
+        {
+            timeValue -= Time.deltaTime;
+        }
+        else
+        {
+            timeValue += 6f;
+            maze.Regenerate();
+        }
+        DisplayTimer(timeValue);
+
+        
+        void DisplayTimer(float timerToDisplay)
+        {
+            if(timerToDisplay < 0f)
+            {
+                timerToDisplay = 0f;
+            }
+
+            float minutes = Mathf.FloorToInt(timerToDisplay / 60);
+            float seconds = Mathf.FloorToInt(timerToDisplay % 60);
+            timer.text = string.Format("{00}:{1:00}", minutes, seconds);
+        }
+    }
+
+
+
+
+
+
+
+}
